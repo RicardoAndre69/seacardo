a83a7c9153cc843225db51619ccc42295b8d52fd
CIEEMG - Ricardo Andre da Silva Filho <ricardo.andre@cieemg.bhz>
2025-11-14 16:39:51 -0300

    changed images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Grocery from '../../assets/grocery.png';
+import MeatSeaFood from  '../../assets/meat-and-seafood.png';
 import Button from '../Button/Button';
 import { useTranslation } from "react-i18next";
 
@@ -13,10 +13,10 @@ const Hero = () => {
                 <div className='flex-1'>
                     <span className='bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'>{t("hero.badge")}</span>
 
-                    <h1 className='text-7xl/20 text-5xl/14 font-bold mt-4'>
+                    <h1 className='text-7xl/20 text-5xl/14 font-bold mt-4 '>
                         {t("hero.title1")}
-                        <span className='text-red-600'> {t("hero.title2")} </span>
-                        <span className='text-sky-300'> {t("hero.title3")} </span>
+                        <span className='text-black'> {t("hero.title2")} </span>
+                        <span className='text-black'> {t("hero.title3")} </span>
                         <br /> In Your City
                     </h1>
                     <p className='text-zinc-600 md:text-lg text-md max-w-[530px] mt-5 mb-10'>
@@ -28,7 +28,7 @@ const Hero = () => {
                 {/* Hero Image */}
 
                 <div className='flex-1'>
-                    <img src={Grocery} alt="Hero Image" />
+                    <img src={MeatSeaFood} alt="Hero Image" />
                 </div>
             </div>
         </section>
